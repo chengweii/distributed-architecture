f8cfa1b53a7d185bb10cedae9746e0deea8b67aa
chengwei2 <chengwei2@jiuxian.com>
2018-06-11 19:23:14 +0800

    init

--- disarch-mq-sdk/src/main/java/com/disarch/mq/consumer/BaseConsumer.java
+++ disarch-mq-sdk/src/main/java/com/disarch/mq/consumer/BaseConsumer.java
@@ -1,23 +1,27 @@
 package com.disarch.mq.consumer;
 
 import com.google.common.base.Throwables;
+import com.rabbitmq.client.Channel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.core.ChannelAwareMessageListener;
 
-public abstract class BaseConsumer {
+public abstract class BaseConsumer implements ChannelAwareMessageListener {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(BaseConsumer.class);
 
-    public void handle(String message) {
+    @Override
+    public void onMessage(Message message, Channel channel) throws Exception {
         LOGGER.info("Receive message: {}", message);
         try {
-            doHandle(message);
+            doHandle(message, channel);
         } catch (Exception e) {
             LOGGER.error("Message: {} with Error: {}", message, e);
             throw Throwables.propagate(e);
         }
     }
 
-    protected abstract void doHandle(String message);
+    protected abstract void doHandle(Message message, Channel channel);
 
 }
--- service/disarch-service/src/main/java/com/disarch/service/mq/OrderSyncConsumer.java
+++ service/disarch-service/src/main/java/com/disarch/service/mq/OrderSyncConsumer.java
@@ -4,10 +4,13 @@ import com.disarch.mq.consumer.BaseConsumer;
 import com.disarch.mq.converter.IMessageConverter;
 import com.disarch.mq.entity.SimpleMessage;
 import com.disarch.mq.service.IMessageService;
+import com.rabbitmq.client.Channel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.Message;
 
 import javax.annotation.Resource;
+import java.io.IOException;
 
 public class OrderSyncConsumer extends BaseConsumer {
     private static final Logger LOGGER = LoggerFactory.getLogger(OrderSyncConsumer.class);
@@ -19,7 +22,18 @@ public class OrderSyncConsumer extends BaseConsumer {
     private IMessageService messageService;
 
     @Override
-    protected void doHandle(String message) {
-        SimpleMessage simpleMessage = messageConverter.deserialize(message, SimpleMessage.class);
+    protected void doHandle(Message message, Channel channel) {
+        try {
+            LOGGER.info("Consum start: {}", message);
+            SimpleMessage simpleMessage = messageConverter.deserialize(String.valueOf(message.getBody()), SimpleMessage.class);
+            channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
+        } catch (Exception e) {
+            LOGGER.error("Consum failed", e);
+            try {
+                channel.basicNack(message.getMessageProperties().getDeliveryTag(), false, false);
+            } catch (IOException e1) {
+                LOGGER.error("Nack failed", e);
+            }
+        }
     }
 }
